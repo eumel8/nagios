53274573b4c3c9873be91dfcccefef3986dc9783
Frank Kloeker <eumel@admin.gnuu.de>
2013-12-10 00:48:51 +0100

    adjust file function for ruby 1.9

--- lib/facter/nagios_ext_services.rb
+++ lib/facter/nagios_ext_services.rb
@@ -4,9 +4,10 @@ Facter.add(:nagios_ext_services) do
      servicelist = Array.new
      nagios_ext_services  = "/etc/nagios/nagios_ext_services.cfg"
      if File.exists?(nagios_ext_services) then
-           File.readlines(nagios_ext_services).each { |line|
-             servicelist << line.chomp
-           } 
+        data = File.open(nagios_ext_services).read
+	data.each_line do  |line|
+	  servicelist << line
+	end
      end 
   end
 end
