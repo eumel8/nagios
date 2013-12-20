94e16e9f12ba14a5434468be06946e7a634002ce
Frank Kloeker <eumel@admin.gnuu.de>
2013-12-20 19:36:51 +0100

    revert disabled checks

--- manifests/command.pp
+++ manifests/command.pp
@@ -26,10 +26,9 @@ class nagios::command {
   nagios_command{'check-host-alive':
     command_line  => '/usr/lib/nagios/plugins/check_ping -H "$HOSTADDRESS$" -w 5000,100% -c 5000,100% -p 1',
   }
-# neuerdings doppelt
-#  nagios_command{'check_tcp':
-#    command_line  => '/usr/lib/nagios/plugins/check_tcp -H $HOSTADDRESS$ -p $ARG1$',
-#  }
+  nagios_command{'check_tcp':
+    command_line  => '/usr/lib/nagios/plugins/check_tcp -H $HOSTADDRESS$ -p $ARG1$',
+  }
   nagios_command{'check_smtp':
     command_line  => '/usr/lib/nagios/plugins/check_smtp -H $HOSTADDRESS$ -p $ARG1$',
   }
@@ -40,10 +39,9 @@ class nagios::command {
     command_line  => '/usr/bin/printf "%b" "***** Nagios *****\n\nNotification Type: $NOTIFICATIONTYPE$\nHost: $HOSTNAME$\nState: $HOSTSTATE$\nAddress: $HOSTADDRESS$\nInfo: $HOSTOUTPUT$\n\nDate/Time: $LONGDATETIME$\n" | /usr/bin/mail -s "** $NOTIFICATIONTYPE$ Host Alert: $HOSTNAME$ is $HOSTSTATE$ **" $CONTACTEMAIL$',
   }
 
-# neuerdings doppelt
-#  nagios_command{'notify-service-by-email':
-#    command_line  => '/usr/bin/printf "%b" "***** Nagios *****\n\nNotification Type: $NOTIFICATIONTYPE$\n\nService: $SERVICEDESC$\nHost: $HOSTALIAS$\nAddress: $HOSTADDRESS$\nState: $SERVICESTATE$\n\nDate/Time: $LONGDATETIME$\n\nAdditional Info:\n\n$SERVICEOUTPUT$\n" | /usr/bin/mail -s "** $NOTIFICATIONTYPE$ Service Alert: $HOSTALIAS$/$SERVICEDESC$ is $SERVICESTATE$ **" $CONTACTEMAIL$',
-#  }
+  nagios_command{'notify-service-by-email':
+    command_line  => '/usr/bin/printf "%b" "***** Nagios *****\n\nNotification Type: $NOTIFICATIONTYPE$\n\nService: $SERVICEDESC$\nHost: $HOSTALIAS$\nAddress: $HOSTADDRESS$\nState: $SERVICESTATE$\n\nDate/Time: $LONGDATETIME$\n\nAdditional Info:\n\n$SERVICEOUTPUT$\n" | /usr/bin/mail -s "** $NOTIFICATIONTYPE$ Service Alert: $HOSTALIAS$/$SERVICEDESC$ is $SERVICESTATE$ **" $CONTACTEMAIL$',
+  }
 
   nagios_command{'notify-host-by-voice':
     command_line  => '/etc/nagios/scripts/twilio_voice.sh "Hello. This is your Network Operation Center. $NOTIFICATIONTYPE$ Host Alert: $HOSTNAME$ is $HOSTSTATE$ **"',
@@ -57,9 +55,9 @@ class nagios::command {
     command_line  => '/etc/nagios/scripts/twilio_sms.sh "NOC Message. $NOTIFICATIONTYPE$ Host Alert: $HOSTNAME$ is $HOSTSTATE$ **"',
   }
 
-#  nagios_command{'notify-service-by-sms':
-#    command_line  => '/etc/nagios/scripts/twilio_sms.sh "NOC Message. $NOTIFICATIONTYPE$ Service Alert: $HOSTALIAS$/$SERVICEDESC$ is $SERVICESTATE$ **"',
-#  }
+  nagios_command{'notify-service-by-sms':
+    command_line  => '/etc/nagios/scripts/twilio_sms.sh "NOC Message. $NOTIFICATIONTYPE$ Service Alert: $HOSTALIAS$/$SERVICEDESC$ is $SERVICESTATE$ **"',
+  }
 
   nagios_command{'process-host-perfdata':
     command_line  => '/usr/bin/printf "%b" "$LASTHOSTCHECK$\t$HOSTNAME$\t$HOSTSTATE$\t$HOSTATTEMPT$\t$HOSTSTATETYPE$\t$HOSTEXECUTIONTIME$\t$HOSTOUTPUT$\t$HOSTPERFDATA$\n" >> /var/lib/nagios3/host-perfdata.out',
--- manifests/contact.pp
+++ manifests/contact.pp
@@ -14,15 +14,15 @@
 
 class nagios::contact {
 
-#  nagios_contact{'email':
-#    service_notification_period     => '24x7',
-#    host_notification_period        => '24x7',
-#    service_notification_options    => 'w,u,c,r',
-#    host_notification_options       => 'd,r',
-#    service_notification_commands   => 'notify-service-by-email',
-#    host_notification_commands      => 'notify-host-by-email',
-#    email                           => 'root@localhost',
-#  }
+  nagios_contact{'email':
+    service_notification_period     => '24x7',
+    host_notification_period        => '24x7',
+    service_notification_options    => 'w,u,c,r',
+    host_notification_options       => 'd,r',
+    service_notification_commands   => 'notify-service-by-email',
+    host_notification_commands      => 'notify-host-by-email',
+    email                           => 'root@localhost',
+  }
   nagios_contact{'sms':
     service_notification_period     => '24x7',
     host_notification_period        => '24x7',
