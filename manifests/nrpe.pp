f4ba76ef53516b5209930e02059172b987815c1b
Frank Kloeker <eumel@admin.gnuu.de>
2014-04-04 19:17:32 +0200

    add plugin mongodb

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -89,14 +89,14 @@ case $::operatingsystem {
   if ($monitor_puppet_agent == 1) {
     cron {'monitor-puppet-agent':
       ensure  => present,
-      command => '/usr/lib/nagios/plugins/check_puppet-agent > /tmp/check_puppet_agent.log',
+      command => '/usr/lib/nagios/plugins/local/check_puppet-agent > /tmp/check_puppet_agent.log',
       user    => 'root',
       minute  => [0,5,10,15,20,25,30,35,40,45,50,55],
       require => File['/usr/lib/nagios/plugins'],
     }
     
     exec {'check_puppet-agent_extra_run':
-      command => '/usr/lib/nagios/plugins/check_puppet-agent > /tmp/check_puppet_agent.log',
+      command => '/usr/lib/nagios/plugins/local/check_puppet-agent > /tmp/check_puppet_agent.log',
       path    => '/bin:/usr/bin:/usr/lib',
       creates => '/tmp/check_puppet_agent.log',
       require => File['/usr/lib/nagios/plugins'],
